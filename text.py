38561307d04456f0a04b4f1f7dca8b0914329b32
Misha <itheckninja203405@gmail.com>
2026-06-16 01:01:44 +0300

    я вернулся

--- text.py
+++ text.py
@@ -1,7 +1,22 @@
-try:
-    number = int(input('введи хуйню'))
-    print(100/number)
-except ValueError:
-    print('хуйня')
-except ZeroDivisionError:
-    print('еблан на ноль делишь')
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+books = [
+    {'title': 'не знаю', 'author': 'хз', 'year': 1922},
+    {'title': 'не знаю2', 'author': 'хз2', 'year': 1934}
+]
+@app.get('/books')
+def get_products():
+    return books
+@app.get('/books/{book_id}')
+def get_book(book_id: int):
+    return books[book_idв]
+class Book(BaseModel):
+    title: str
+    author: str
+    year: int
+@app.post('/books')
+def create_book(book: Book):
+    books.append({'title': book.title, 'author': book.author, 'year': book.year})
+    return {'message': f'{book.title} was added'}
